e7dbb51058d7c1645ceff9783fabaaa8576befa0
Tyler Baker <tyler.baker@gcpsk12.org>
2026-05-14 15:21:55 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+enum Command { 
+}
+
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
     io::stdout().flush().unwrap();
+    println!("{}: command not found", command.trim());
 }
+
+
