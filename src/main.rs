bf25f1586e9191e8121b5a110c627ca2299a064c
Tyler Baker <tyler.baker@gcpsk12.org>
2026-05-14 15:51:14 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,12 @@ enum Command {
 fn main() {
     loop {
         print_string("$ ");
-        let command = read_input();
+        let input = read_input();
+        let args: Vec<&str> = input.split(" ").collect();
+        let command = args[0];
         let message = match command.trim().to_string().as_str() { 
             "exit" => break,
+            "echo" => format!("{}", args[1..].join(" ")),
             _ => format!("{}: command not found", command.trim())
         };
         print_string(&message);
