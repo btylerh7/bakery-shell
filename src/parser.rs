8a4ca42acc2395f0e824bf5a76873ddfce643c18
Tyler Baker <codeberg@robustjumprope.lol>
2026-05-19 10:43:01 -0400

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -1,47 +1,78 @@
+
+#[derive(Clone, Debug, PartialEq)]
 enum ParserState {
     SingleQuote,
     NoQuote,
 }
 
+#[derive(Clone, Debug)]
+struct Arg {
+    value: String,
+    state: ParserState
+}
+impl Arg {
+    fn new() -> Self {
+        return Arg {
+            value: String::new(),
+            state: ParserState::NoQuote
+        }
+    }
+}
+
 pub fn parse_arg_string(input: &str) -> Vec<String> {
-    let mut current_state = ParserState::NoQuote;
-    let mut current_arg = String::new();
+    let mut current_arg = Arg::new();
+    let mut args: Vec<Arg> = vec![];
     let mut previous_char: Option<char> = None;
-    let mut result: Vec<String> = vec![];
+    let trimmed_input = input.trim();
+    println!("Input to parser was {}", input.trim());
 
-
-    for thing in input.chars() {
+    for thing in trimmed_input.chars() {
         match thing {
-            '\'' => match current_state {
-                ParserState::NoQuote => {
-                    if let Some(prev) = previous_char {
-                        if prev != '\'' {
-                            result.push(current_arg.clone());
-                            current_arg = String::new();
-                        }
-                        current_state = ParserState::SingleQuote;
+            // If the last character was a ', the current character is a ', and
+            // it is the start of a new pair of ', concatenate the strings
+            '\'' if let Some(prev) = previous_char && prev == '\'' => {
+                if let Some(prev) = args.last() && prev.state == ParserState::SingleQuote {
+                    let prev_arg = args.pop().unwrap();
+                    current_arg.value = prev_arg.value;
+                    current_arg.state = ParserState::SingleQuote;
+                }
+            },
+            '\'' => {
+                args.push(current_arg.clone());
+                current_arg = Arg::new();
+                current_arg.state = match current_arg.state {
+                    ParserState::SingleQuote => ParserState::NoQuote,
+                    ParserState::NoQuote => ParserState::SingleQuote
+                };
+            },
+            thing if thing.is_whitespace() => {
+                match current_arg.state {
+                    ParserState::SingleQuote => current_arg.value.push(thing),
+                    ParserState::NoQuote => {
+                        args.push(current_arg.clone());
+                        current_arg = Arg::new();
                     }
                 }
-                ParserState::SingleQuote => {
-                    result.push(current_arg.clone());
-                    current_state = ParserState::NoQuote;
-                    current_arg = String::new();
-                }
             },
-            thing if thing.is_whitespace() => match current_state {
-                ParserState::SingleQuote => current_arg.push(thing),
-                ParserState::NoQuote => {
-                    result.push(current_arg.clone());
-                    current_arg = String::new();
-                }
-            },
-            _ => current_arg.push(thing),
+            _ => {
+                current_arg.value.push(thing);
+            }
         }
         previous_char = Some(thing.clone());
     }
-
-    result
+    if !current_arg.value.is_empty() {
+        args.push(current_arg);
+    }
+    let result = args
         .into_iter()
-        .filter(|result_string| !result_string.is_empty())
-        .collect()
+        .map(|arg| {
+            arg.value.trim().to_string()
+        })
+        .filter(|arg| {
+            !arg.is_empty()
+        })
+        .collect();
+    result
+
 }
+
